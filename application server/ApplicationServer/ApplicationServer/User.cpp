da7886f4d8e364e79a1dee00ff646b3b2389ed28
Julien Loève (home) <julien.loeve@imerir.com>
2014-10-07 23:13:31 +0200

    Correcting User.cpp
    Signed-off-by: Julien Loève (home) <julien.loeve@imerir.com>

--- application server/ApplicationServer/ApplicationServer/User.cpp
+++ application server/ApplicationServer/ApplicationServer/User.cpp
@@ -1,43 +1,44 @@
-#include "client.h"
+#include "User.h"
 
 
-client::client()
+User::User()
 {
 }
 
-void client::setToken(std::string tok)
+void User::setToken(utility::string_t tok)
 {
 	this->token = tok;
 }
-std::string client::getToken() const
+utility::string_t User::getToken() const
 {
 	return this->token;
 }
-void client::setLstConnection(std::time_t tm)
+void User::setLstConnection(std::time_t tm)
 {
 	this->lastConnection = tm;
 }
-std::time_t client::getLstConnection() const
+std::time_t User::getLstConnection() const
 {
 	return this->lastConnection;
 }
-void client::setStates(int state)
+void User::setStates(int state)
 {
 	this->state = state;
 }
-int client::getStates() const
+int User::getStates() const
 {
 	return this->state;
 }
-void client::setLstPositionKnown(Point coordinates)
+void User::setLstPositionKnown(Point coordinates)
 {
 	this->lastPositionKnown = coordinates;
 }
-Point client::getLstPositionKnown() const
-{
-	return this->lastPositionKnown;
+Point User::getLstPositionKnown() const
+{ 
+	return Point::Point(12.2f, 3.5f, 0.f); // For tests purpose only
+	//return this->lastPositionKnown;
 }
 
-client::~client()
+User::~User()
 {
 }
--- application server/ApplicationServer/ApplicationServer/user.h
+++ application server/ApplicationServer/ApplicationServer/user.h
@@ -8,14 +8,14 @@ class User
 public:
 	User();
 	~User();
-	void setToken(std::string);
-	utility::string_t getToken() const{ return(U("slfdghjfe")); };
+	void setToken(utility::string_t);
+	utility::string_t getToken() const;
 	void setLstConnection(std::time_t);
 	std::time_t getLstConnection() const;
 	void setStates(int);
 	int getStates() const;
 	void setLstPositionKnown(Point);
-	Point getLstPositionKnown() const { return Point::Point(12.2f, 3.5f, 0.f); };
+	Point getLstPositionKnown() const;
 	int getDistance() const { return 12; };
 	void setDistance(int);
 	bool isAt(Point){ return true; };
@@ -24,7 +24,7 @@ public:
 
 private:
 	int distance;
-	std::string token;
+	utility::string_t token;
 	std::time_t lastConnection;
 	int state;
 	Point lastPositionKnown;
